16ee1a35f71b0c84fc5041fdd11dd0aa03e4863e
KUZHANTHAIVASAN R <148938821+kuzhanthaivasan@users.noreply.github.com>
2025-04-08 15:38:14 +0530

    app.js

--- app.js
+++ app.js
@@ -112,13 +112,6 @@ connectDB().catch(err => {
 });
 
 
-// -------------------- STATIC BUILD (FOR PRODUCTION) --------------------
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
 
 // -------------------- HEALTH CHECK --------------------
 app.get('/health', (req, res) => {
